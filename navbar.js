68c81217c1ae49d8d5cffae1a60766eb3f1ccfec
ggsacoi <118943639+ggsacoi@users.noreply.github.com>
2025-05-16 23:37:41 +0200

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -187,13 +187,14 @@ section.addEventListener("click", () => {
        {value:'silver armor', url:"again8.html"},
        {value:'Balenciaga silver', url:"again9.html"},
        {value:'medusa biggie noir', url:"again10.html"},
-       {value:'louis vuitton Acetate 1.1 Millionaires blanc', url:"again11.html"},
+       {value:'louis vuitton Acetate 1.1 Millionaires blanc', url:"again18.html"},
        {value:'gaming esp white', url: "again12.html"},
        {value:'call of duty MW', url:"again13.html"},
        {value:'rainbow six siege', url:"again14.html"},
        {value:'starkIndustries', url:"again15.html"},
        {value:'gaming esp orange', url:"again16.html"},
        {value:'dolce & gabbana', url:"again17.html"},
+        {value:'League of legends', url:"again11.html"},
     ];
 
     const resultBox = document.querySelector('.things');
