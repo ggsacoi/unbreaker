858f28a1ebe44505e4516a8e69728c95c863bd58
ggsacoi <118943639+ggsacoi@users.noreply.github.com>
2026-04-01 17:20:32 +0200

    ecomm

--- navbar.js
+++ navbar.js
@@ -105,7 +105,7 @@ async function initializeNavbar() {
         list.className = "listoptions";
         const user = currentUser?.uid ? users.find(u => u.uid === currentUser.uid) : undefined;
         const lielement = [
-            {name:users?.firstName, url:"Mervie.html"},
+            {name:u?.firstName, url:"Mervie.html"},
             {name:'COMPTE', url:"set.html"},
             {name:'PRINCIPAL', url:"index.html"},
             {name:'DE SOLEIL', url:"catalogue.html"},
